bb78118fa56baa20802cc7c5d4cccebeb7f2f6b5
Onur Kapucu <kapucuonur@hotmail.comcat>
2025-03-08 02:00:00 +0200

    updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,9 +125,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / "frontend/build/static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
