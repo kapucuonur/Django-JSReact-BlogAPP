f8f1e37e7b7e3ccfeaff57cdabfc738b34a4cb04
Onur Kapucu <kapucuonur@hotmail.com>
2025-03-10 17:42:57 +0200

    updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,7 +94,7 @@ CORS_ORIGIN_WHITELIST = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
